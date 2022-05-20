c9742b6abbfe482f60b2456d270f2cc84896d33a
fgkl <105131570+fgkl@users.noreply.github.com>
2022-05-20 19:53:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const Discord = require('discord.js')
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 const token = 'OTc2OTAxMzYwMzA4MDgwNjgx.GG_s93.yILpFPBRPXCoUbID2mXf88m7fzcbtImz9KasMw'
-const prefix = "-"
+const prefix = "!"
 const request = require('request');
 const path = require('path');
 const fetch = require('node-fetch');
@@ -18,75 +18,66 @@ client.once('ready', async () => {
     type: "WATCHING"
   });
 });
+
 client.on('messageCreate', async (message) => {
-  
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  if (message.author.bot) return;
-  
-  // inside a command, event listener, etc.
-  const exampleEmbed = new MessageEmbed()
-    .setColor('#0099ff')
-    .setTitle('Obfuscated script.')
-    .setAuthor({ name: 'Soloware™ obfuscator', iconURL: message.author.avatarURL, url: message.author.avatarURL })
-    .setTimestamp()
-  //
-  
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
 
-  //
-  const file = message.attachments.first()?.url;
-  if (!file) return console.log('No attached file found');
-  //
-  try {
-    const response = await fetch(file);
-    if (!response.ok)
-      return message.channel.send(
-        'There was an error with fetching the file:',
-        response.statusText,
-      );
-    console.log(message.author.toJSON())
+  if (command === 'obf') {
+
+
+    if (message.author.bot) return;
+
+    // inside a command, event listener, etc.
+    const exampleEmbed = new MessageEmbed()
+      .setColor('#0099ff')
+      .setTitle('Obfuscated script.')
+      .setAuthor({ name: 'Soloware™ obfuscator', iconURL: message.author.avatarURL, url: message.author.avatarURL })
+      .setTimestamp()
     //
-    const text = await response.text();
-    const f = fs.writeFileSync('ob.lua', text)
-    if (text) {
-      require('./d.js').obfuscate(
-        fs.readFileSync('./ob.lua').toString()
-      ).then(([outputPath, settings]) => {
-        console.log(`saved to '${outputPath}'`)
-        message.reply({
-          embeds: [exampleEmbed],
-        })
-        message.channel.send({
-          
-          files: [
-            "./temp/output-1.lua"
-            
-          ],
-        }); 
+
+
+    //
+    const file = message.attachments.first()?.url;
+    if (!file) return console.log('No attached file found');
+    //
+    try {
+      const response = await fetch(file);
+      if (!response.ok)
+        return message.channel.send(
+          'There was an error with fetching the file:',
+          response.statusText,
+        );
+      console.log(message.author.toJSON())
       //
-      }).catch((err) => {
-        console.log("FAIOIIL", err)
-      })
+      const text = await response.text();
+      const f = fs.writeFileSync('ob.lua', text)
+      if (text) {
+        require('./d.js').obfuscate(
+          fs.readFileSync('./ob.lua').toString()
+        ).then(([outputPath, settings]) => {
+          console.log(`saved to '${outputPath}'`)
+          message.reply({
+            embeds: [exampleEmbed],
+          })
+          message.channel.send({
+
+            files: [
+              "./temp/output-1.lua"
+
+            ],
+          });
+          //
+        }).catch((err) => {
+          console.log("FAIOIIL", err)
+        })
+      }
+      //
+    } catch (error) {
+      console.log(error);
     }
-  //
-  } catch (error) {
-    console.log(error);
   }
-})
-//
-
-client.on("messageDelete", async (message) => {
-    message.channel.send({
-      files:[
-        "./vids/dgmlmao_20220514_4.mp4"
-      ]
-    })
-
-})
-
-//
-client.on("rateLimit",async (cool) =>{
-   cool.channel.send({
-     content: "Bot is at ratelimit"
-   })
 })
 client.login(token);
